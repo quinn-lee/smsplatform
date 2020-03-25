ddf29334131b92f4d19f254b680c227a33956fd8
quinn-lee <lifuyuan33@hotmail.com>
2020-03-26 04:40:54 +0800

    测试脚本

--- main/api_1_0/views.py
+++ main/api_1_0/views.py
@@ -2,6 +2,7 @@
 from . import api
 from main import models
 from flask import current_app
+import time
 
 
 @api.route("/")
@@ -11,3 +12,14 @@ def index():
     current_app.logger.info("info")
     current_app.logger.debug("debug")
     return "<h1>this api_1_0</h1>"
+
+
+@api.route('/test')
+def maintest():
+    return 'hello world'
+
+
+@api.route('/sleep')
+def mainsleep():
+    time.sleep(10)
+    return 'wake up'
